e7e02b6dfb7b8887dde3feba18101d662a937f51
euhoo <euhoo@mail.ru>
2019-05-08 09:22:07 +0300

    add new channel react component

--- src/components/channels.jsx
+++ src/components/channels.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 
-const Channels = (props) => {
-  const { channels } = props;
-  return (
-    <div>
-      {channels.map(channel => <button className="btn btn-outline-success" type="button" key={channel.id}>{channel.name}</button>)}
-      <button className="btn btn-outline-dark w-100" type="button">New</button>
-      <form>
-        <div className="input-group mb-3">
-          <input type="text" className="form-control" placeholder="Имя нового канала" aria-label="Текст" aria-describedby="basic-addon2" />
-        </div>
-      </form>
-    </div>
-  );
-};
-export default Channels;
+export default class Channels extends React.Component {
+  state = { visible: false }
+
+  onButton = (e) => {
+    //e.preventDefault();
+    const { visible } = this.state;
+    console.log(visible);
+    this.setState({ visible: !visible });
+  }
+
+  onClick = (e) => {
+    //e.preventDefault();
+    alert('sdf');
+  }
+
+  render() {
+    const { channels } = this.props;
+    const { visible } = this.state;
+    const styleInput = visible ? { display: '' } : { display: 'none' };
+    const style = visible ? { display: 'none' } : { display: '' };
+    return (
+      <div>
+        {channels.map(channel => <button className="btn btn-outline-success w-100" type="button" key={channel.id}>{channel.name}</button>)}
+        <button className="btn btn-outline-dark w-100" type="button" style={style} onClick={this.onButton}>New</button>
+        <form>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" style={styleInput} placeholder="Имя нового канала" aria-label="Текст" aria-describedby="basic-addon2" />
+            <button className="btn btn-outline-dark" type="button" style={styleInput} onClick={this.onButton}>Add</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
