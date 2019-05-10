3f779301f34691a88259129d8c7a9680278c559a
euhoo <euhoo@mail.ru>
2019-05-10 16:36:33 +0300

    fix reduxDevTools bug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import gon from 'gon';
 // import faker from 'faker';
 // import cookies from 'js-cookie';
 import io from 'socket.io-client';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import chat from './components/index';
 import normalize from './utils/normalize';
@@ -15,17 +15,11 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-// eslint-disable-next-line no-underscore-dangle
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__;
-
 const normilizedStore = normalize(gon);
 const store = createStore(
   reducers,
   normilizedStore,
-  compose(
-    applyMiddleware(thunk),
-    reduxDevtools && reduxDevtools(),
-  ),
+  applyMiddleware(thunk),
 );
 chat(store);
 const socket = io();
