9229c5484f738e5a5eb642ce16dcb9ee54026350
euhoo <euhoo@mail.ru>
2019-05-13 23:33:33 +0300

    context works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
 import gon from 'gon';
-// import faker from 'faker';
-// import cookies from 'js-cookie';
+import faker from 'faker';
+import cookies from 'js-cookie';
 import io from 'socket.io-client';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -12,7 +12,6 @@ import {
   newChannel, removeChannel, renameChannel, addMessage,
 } from './utils/serverAnswerFunctions';
 import reducers from './reducers';
-import { userName } from './utils/appContext';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
@@ -25,7 +24,12 @@ const store = createStore(
   applyMiddleware(thunk),
 );
 
-chat(store, userName);
+if (!cookies.get('name')) {
+  const randomName = faker.name.findName();
+  cookies.set('name', randomName);
+}
+
+chat(store);
 const socket = io();
 socket.on('newChannel', data => newChannel(data, store));
 socket.on('newMessage', data => addMessage(data, store));
--- src/utils/appContext.js
+++ src/utils/appContext.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import faker from 'faker';
 import cookies from 'js-cookie';
 import queries from './queries';
 
-
-if (!cookies.get('name')) {
-  const randomName = faker.name.findName();
-  cookies.set('name', randomName);
-}
-export const userName = cookies.get('name');
-
-
-export default React.createContext({ userName, queries });
+export default React.createContext({ userName: cookies.get('name'), queries });
