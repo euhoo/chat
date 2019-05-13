80142783f3d2074e5a6a105866118b6066a1e8c2
euhoo <euhoo@mail.ru>
2019-05-13 23:39:27 +0300

    context fix

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import cookies from 'js-cookie';
 import App from './App';
+import AppContext from '../utils/appContext';
+import queries from '../utils/queries';
+
+const context = { userName: cookies.get('name'), queries };
 
 export default (store) => {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <AppContext.Provider value={context}>
+        <App />
+      </AppContext.Provider>
     </Provider>,
     document.getElementById('chat'),
   );
--- src/utils/appContext.js
+++ src/utils/appContext.js
@@ -1,5 +1,3 @@
 import React from 'react';
-import cookies from 'js-cookie';
-import queries from './queries';
 
-export default React.createContext({ userName: cookies.get('name'), queries });
+export default React.createContext();
